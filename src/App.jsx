8514809cc553cb92763d13ea2dafcae3a6d04618
ikeshav26 <keshavgilhotra4@gmail.com>
2025-05-06 16:19:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import bgImage from '../public/bg.png'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from "../src/pages/LandingPage"
+import Contact from "../src/pages/Contact"
+import Discover from "../src/pages/Discover"
+import Home from "../src/pages/Home"
+import Projects from "../src/pages/Projects"
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/discover" element={<Discover/>}></Route>
+        <Route path="/projects" element={<Projects/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import bgImage from '../../public/bg.png'
+
+const LandingPage = () => {
+  return (
+    <div style={{ backgroundImage: `url(${bgImage})` }} className="bg-cover bg-center h-screen bg-full">
+      landing page
+    </div>
+  )
+}
+
+export default LandingPage
